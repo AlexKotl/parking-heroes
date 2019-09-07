ee83aa6df600d48a1b6f197a04274ec4da1ac4b9
Alex <slicer256@gmail.com>
2019-09-07 22:53:05 +0300

    keyboard refactor

--- parking.py
+++ parking.py
@@ -1,7 +1,6 @@
 import os
 import telebot
 from queries import Queries 
-
 BOT_TOKEN = os.environ['BOT_TOKEN']
 
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -9,16 +8,25 @@ bot = telebot.TeleBot(BOT_TOKEN)
 queries = Queries()
 for row in queries.get_all_parking():
     print(row)
+    
+keyboard_buttons = {
+    "report": "🚨 Сообщить о нарушителе",
+    "list": "🅿️ Список нарушителей", 
+    "details": "🚙 Информация по номеру",
+    "about": "ℹ️ О боте",
+}
 
 def create_keyboard():
     keyboard = telebot.types.ReplyKeyboardMarkup(row_width=2)
-    buttons = [
-        "🅿️ Список нарушителей", 
-        "ℹ️ Информация",
-        "🚨 Сообщить о нарушителе",
-    ]
-    keyboard.add(*buttons)
+    keyboard.add(*keyboard_buttons.values())
     return keyboard
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	bot.send_message(chat_id=message.chat.id, 
+        text='Привет! Здесь вы сможете пожаловаться на нарушителей правил паровки ЖК "Сырецкий Бояр".'
+        'Возспользуйтесь кнопками внизу, чтобы добавить нарушителя или просмотреть список "героев".', 
+        reply_markup=create_keyboard())
 
 @bot.message_handler()
 def handle_message(message):
