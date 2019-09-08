83b1fce17aaa0cc68d78112a596601478721859e
Alex <slicer256@gmail.com>
2019-09-08 17:52:59 +0300

    additional queries

--- queries.py
+++ queries.py
@@ -9,6 +9,9 @@ class Queries:
     
     def get_parking_by_plate(self, plate):
         return self.db.get_rows("SELECT * FROM parking WHERE car_plate = %s ORDER BY id DESC", (plate,))
+    
+    def get_latest_parking_by_user(self, id):
+        return self.db.get_row("SELECT * FROM parking WHERE user_id = %s ORDER BY id DESC LIMIT 1", (id,))
 
     def add_parking(self, **kwargs):
         data = {
@@ -19,3 +22,6 @@ class Queries:
             'user_last_name': kwargs['user_last_name'],
         }
         return self.db.insert('parking', data)
+
+    def add_parking_description(self, id, description):
+        return self.db.update('parking', id, { 'description': description })
