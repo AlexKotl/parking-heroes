dd8474e10d05ddedf0cafa429c9b22cd495c351a
Alex <slicer256@gmail.com>
2019-09-08 23:37:28 +0300

    proper plate format

--- plate.py
+++ plate.py
@@ -19,14 +19,23 @@ class Plate:
     
     def format_plate(self, plate):
         ''' Clean raw plate number from foreign symbols and convert latin to cyr ''' 
+        # convert to upper
+        plate = plate.upper()
+        
+        # convert latin to cyr letters
+        plate = self.latin_to_cyr(plate)
         
         # strip not used symbols
-        plate = re.sub(r'[^а-яА-Я0-9І]', '', plate)
+        plate = re.sub(r'[^А-Я0-9І]', '', plate)
+        
+        # test for proper regexp
+        if not re.match(r"([А-ЯІ]{0,2})([0-9]{4,5}){1}([А-ЯІ]{0,2})", plate) or len(plate) > 8:
+            return False
         
         return plate
-        
-    def is_plate(self, str):
-        ''' Check if string is car plate format '''
-        pass
-        
     
+    def latin_to_cyr(self, str):
+        for ch in self.translate_rules.keys():
+            if ch in str:
+                str = str.replace(ch, self.translate_rules[ch])
+        return str
