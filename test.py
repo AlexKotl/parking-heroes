5604d89986a7405d2eb778f6addfa80dd4ba9927
Alex <slicer256@gmail.com>
2019-09-08 13:58:46 +0300

    minor changes

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from plate import Plate
 class TestPlate(Plate, unittest.TestCase):
     def test_format(self):
         self.assertEqual(self.format_plate('АА 2203 ІК'), 'АА2203ІК')
+        self.assertEqual(self.format_plate('аа 2203 аа'), 'АА2203ІК')
         self.assertEqual(self.format_plate('- АА2203ІК,'), 'АА2203ІК')
         self.assertEqual(self.format_plate('2203'), '2203')
         self.assertEqual(self.format_plate('К 2203 НН'), 'К2203НН') # old format
