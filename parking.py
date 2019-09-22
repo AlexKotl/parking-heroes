6f043d80c27193144a8b8e09dd0f1484b42fd8d2
Alex <slicer256@gmail.com>
2019-09-22 23:17:27 +0300

    show plate info function moved to another file

--- parking.py
+++ parking.py
@@ -112,29 +112,7 @@ def handle_message(message):
 @bot.message_handler(func=lambda message: get_step(message) == STEP_PLATE_INFO or (getattr(message, 'text')!=None and getattr(message, 'text')[:2] == '/_'))
 @log_message
 def handle_message(message):
-    ''' Send info about specific plate no '''
-    if message.text[:2] == '/_':
-        number = plate.format_plate(message.text[2:])
-    else:
-        number = plate.format_plate(message.text)
-    reply = ''
-    photos = []
-    
-    if number == False:
-        reply = f'Введенный вами номер `{message.text}` не распознан как автомобильный номер. Попробуйте снова:'
-    else:
-        rows = repo.get_parking_by_plate(number)
-        if rows == False:
-            reply = f"Записей по номеру `{number}` *не найдено*. Похоже автомобиль не нарушал правил парковки."
-        else:
-            reply = f"Найденные записи по номеру `{number}`: \n\n"
-            for row in rows:
-                reply += f" ❗️ {row['description']} (_{row['date_created'].date()}_) \n"
-                photos.append(row['photo'])
-        set_step(message, STEP_DEFAULT)
-    bot.send_message(chat_id=message.chat.id, text=reply, reply_markup=create_keyboard(), parse_mode='Markdown')
-    for photo in photos[:4]:
-        send_photo(message, photo)
+    parking_details.display_plate_details(message, plate, repo, set_step, bot, send_photo, create_keyboard)
 
 # ADD CAR
 
